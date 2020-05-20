dd1d72317e9b141d46dd0138479076c1dcbca98c
kom christian <kirasteph96@gmail.com>
2020-05-20 11:44:00 +0100

    camgpsv3.1.0

--- resources/views/layouts/components/header.blade.php
+++
@@ -1,5 +0,0 @@
-        <link rel="stylesheet" href="{{ url('/css/all.css') }}">
-        <link rel="stylesheet" href="{{ url('/css/bootstrap.css') }}">
-        <link rel="stylesheet" href="{{ url('/css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ url('/css/custom.css') }}">
-        <link rel="stylesheet" href="{{ url('/css/my-login.css') }}">
