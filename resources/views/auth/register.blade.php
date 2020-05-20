82ebdff94331b5ea4060670ca1177e831efdf688
kom christian <kirasteph96@gmail.com>
2020-05-20 12:07:22 +0100

    camgpsv5.0.1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
         <link rel="stylesheet" href="{{ url('/css/login.css') }}">
 
     </head>
-<body class="body">
+<body class="bodye">
     <div class="wave-container" style="position:absolute;z-index:0">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path fill="#4a346b" fill-opacity="1" d="M0,192L60,165.3C120,139,240,85,360,64C480,43,600,53,720,90.7C840,128,960,192,1080,208C1200,224,1320,192,1380,176L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
@@ -95,7 +95,7 @@
 									</button>
 								</div>
 								<div class="mt-4 text-center">
-									Already have an account? <a href="index.html">Login</a>
+									Already have an account? <a href="/login">Login</a>
 								</div>
 							</form>
 						</div>
